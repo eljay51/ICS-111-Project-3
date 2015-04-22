cd1454a0073fca8c5901ce9a200095e44d792283
Elmo <eg21@hawaii.edu>
2015-04-22 00:55:42 -1000

    Pictures and code

---
+++ src/Drone.java
@@ -0,0 +1,73 @@
+import java.util.Random;
+
+// The drone class will control the artificial intelligence of the drone for the Drone vs Chicken program
+// You will begin development with the go() member function.
+
+public class Drone extends Agent {
+
+	Chicken[] chickens;			// This array contains all the chickens
+	int numberOfChickens;		// This tells you how many chickens there are
+	
+	// Constructor for drone
+	Drone() {
+		
+		// Place the drone at random locations
+		Random randomGenerator = new Random();
+		x = randomGenerator.nextInt(1024);
+		y = randomGenerator.nextInt(768);
+		picture = EZ.addImage("drone.png", x, y);
+	}
+	
+	// Assign array of chickens to drone
+	void introduceChickensToDrone(Chicken[] chk, int nchickens){
+		chickens = chk;
+		numberOfChickens = nchickens;
+	}
+	
+	void setImagePosition(int posx, int posy) {
+		super.setImagePosition(posx, posy);
+		picture.rotateBy(1);
+	}
+	
+	// Here is where you begin to put the intelligence for your drones.
+	// Call this repeatedly to keep the drone updated.
+	int prevx=0;
+	int prevy=0;
+	
+	void go(){
+		
+		if (x > 1024) moveLeft(1);
+		if (x < 0) moveRight(1);
+		if (y > 768) moveUp(1);
+		if (y < 0) moveDown(1);
+		move();
+//		if((x<= 1024 && x>=0)&&(y<=768 && y>=0)){
+////			setImagePosition(x, y);
+//			
+//			move();
+//			prevx=x+1;
+//			prevy=y+1;
+//			
+//			System.out.println("X: "+x+" Y: "+y);
+//			
+//		}else{
+//			setPosition(prevx,prevy);
+//		}
+		
+	}
+	
+	void move(){
+		if(EZInteraction.isKeyDown('s')){
+			setImagePosition(x,y++);
+		}
+		if(EZInteraction.isKeyDown('w')){
+			setImagePosition(x,y--);
+		}
+		if(EZInteraction.isKeyDown('a')){
+			setImagePosition(x--,y);
+		}
+		if(EZInteraction.isKeyDown('d')){
+			setImagePosition(x++,y);
+		}
+	}
+}
--- src/main.java
+++ src/main.java
@@ -1,11 +1,32 @@
+import java.awt.Color;
 
 public class main {
 
 	public static void main(String[] args) {
 		
 		EZ.initialize(1024,768);
+		EZ.addImage("field.jpg",512,384);
+		Chicken[] chicken = new Chicken[10];
+		Drone drone = new Drone();
+		Coop coop= new Coop();
 		
-
+		drone.setPosition(0,384);
+		
+		
+		for(int i=0;i<chicken.length;i++){
+			chicken[i]= new Chicken();
+		}
+		
+		while(true){
+			for (int i=0;i<chicken.length;i++){
+				chicken[i].go();
+			}
+		drone.go();
+		
+		EZText score= EZ.addText(884,500,""+ coop.numberOfChickensInCoop(),Color.BLACK,51);
+		
+		EZ.refreshScreen();
+		}
 	}
 
 }
